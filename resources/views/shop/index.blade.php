42ff5c61391a6a588f1cef7be4f55d0e72e6bbaf
Edwyn Jones <edwyn__4@hotmail.com>
2020-03-27 13:25:49 +0100

    small update

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -52,4 +52,54 @@
             </div>
         </div>
     </div>
+
+    <br>
+
+    <div class="row">
+        <div class="col-sm-6 col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="https://bit.ly/2JklgO0" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <div class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </div>
+                    <div class="button">
+                        <div class="float-left price"> €12 </div>
+                        <a href="#" class="btn btn-primary float-right"> Add to cart </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="https://bit.ly/2JklgO0" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <div class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </div>
+                    <div class="button">
+                        <div class="float-left price"> €12 </div>
+                        <a href="#" class="btn btn-primary float-right"> Add to cart </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+            <div class="card" style="width: 18rem;">
+                <img src="https://bit.ly/2JklgO0" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <div class="card-text">
+                        Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </div>
+                    <div class="button">
+                        <div class="float-left price"> €12 </div>
+                        <a href="#" class="btn btn-primary float-right"> Add to cart </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
