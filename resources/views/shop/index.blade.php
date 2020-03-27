c3c3f7450ed5860b1ea636154a95456bc7a7544a
Edwyn Jones <edwyn__4@hotmail.com>
2020-03-27 12:28:54 +0100

    Everything right now is with bootstrap. I will later change the styling to my own styling with SASS.

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title')
+    Laravel Cantine Choupi
+@endsection
+
+@section('content')
+    <div class="card" style="width: 18rem;">
+        <img src="https://bit.ly/2JklgO0" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <div class="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+            </div>
+            <div class="button">
+                <a href="#" class="btn btn-primary"> Add to cart </a>
+            </div>
+        </div>
+    </div>
+@endsection
